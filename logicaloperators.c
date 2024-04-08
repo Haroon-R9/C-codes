262397227b44356a5641551d1fe97b07e0a12693
Haroon <haroonrind8@gmail.com>
2024-04-08 08:43:31 +0000

    operators

---
+++ logicaloperators.c
@@ -0,0 +1,10 @@
+include<stdio.h>
+                    // any non zero value is considered to be true
+int main(){
+    int a = 11;
+    int b = 3;
+    if(a>9 && b<5){
+        printf("yes");
+    }
+    return 0;
+}
